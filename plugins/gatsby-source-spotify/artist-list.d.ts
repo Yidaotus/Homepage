72dd2800c7bb955b5f02e577f7d6e29a437526a3
Daniel Voigt <yidaotus@protonmail.com>
2021-11-17 20:13:17 +0100

    configure statics

--- plugins/gatsby-source-spotify/artist-list.d.ts
+++
@@ -1,3 +0,0 @@
-import { Artist as RecentArtist } from './types/spotify-recent';
-import { Artist } from './types/spotify-top-tracks';
-export declare const generateArtistString: (artists: Artist[] | RecentArtist[]) => string;
